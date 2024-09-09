54f0c6822ceaa38fb22b3c066246fbff5a32605d
Mahardika Bayu Rahmadi <mahardika.bayu.rahmadi@students.untidar.ac.id>
2024-09-09 20:11:15 +0700

    Menambahkan file java

---
+++ tugas/Mobil.java
@@ -0,0 +1,62 @@
+package tugas;
+
+public class Mobil {
+    private String merk;
+    private String model;
+    private int tahun;
+    private String warna; // Atribut baru
+
+    // Constructor
+    public Mobil(String merk, String model, int tahun, String warna) {
+        this.merk = merk;
+        this.model = model;
+        this.tahun = tahun;
+        this.warna = warna;
+    }
+
+    // Getter dan Setter
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getTahun() {
+        return tahun;
+    }
+
+    public void setTahun(int tahun) {
+        this.tahun = tahun;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    // Method untuk menampilkan informasi mobil
+    public void displayInfo() {
+        System.out.println("Merk: " + merk);
+        System.out.println("Model: " + model);
+        System.out.println("Tahun: " + tahun);
+        System.out.println("Warna: " + warna);
+    }
+
+    // Method untuk menyalakan mesin
+    public void startEngine() {
+        System.out.println("Mesin mobil " + merk + " menyala");
+    }
+}
