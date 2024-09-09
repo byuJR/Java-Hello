1d65191e104e3fb91d033b8ae26375e0273c5083
Mahardika Bayu Rahmadi <149688748+byuJR@users.noreply.github.com>
2024-09-09 20:24:46 +0700

    Add files via upload

---
+++ Projek1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projek1;
+
+/**
+ *
+ * @author byu
+ */
+public class Projek1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama saya Bayu");
+        System.out.println("NPM saya 2320506029");
+        System.out.println("Asal saya Kebumen");
+        System.out.println("No Hp saya 08001776565");
+    }
+}
